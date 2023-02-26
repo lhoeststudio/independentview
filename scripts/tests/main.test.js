c695567d9b9a776d9783945ffc507735a5dfbc9b
Nicholas Lhoest <nicholaslhoest@gmail.com>
2023-02-26 15:07:40 +0100

    Test - Add Tests

---
+++ scripts/tests/main.test.js
@@ -0,0 +1,11 @@
+const jsdom = require("jsdom");
+
+describe("Element Amounts", () => {
+
+    it("Should Return Correct Number of Items in the List", () => {
+        var myElements = 5;
+
+        expect(myElements).toBe(5);
+    })
+
+});
